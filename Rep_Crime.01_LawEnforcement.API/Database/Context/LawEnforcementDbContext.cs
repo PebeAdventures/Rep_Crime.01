81b7b0e3d04fdd16ea4cfd83c7175955077289b6
Piotr Bujak <piotr.bujak@motorolasolutions.com>
2022-10-24 16:30:58 +0200

    Add LawEnforcement DbContext

---
+++ Rep_Crime.01_LawEnforcement.API/Database/Context/LawEnforcementDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Rep_Crime._01_LawEnforcement.API.Models;
+
+namespace Rep_Crime._01_LawEnforcement.API.Database.Context
+{
+    public class LawEnforcementDbContext : DbContext
+    {
+
+        public DbSet<LawEnforcement> lawEnforcements { get; set; }
+
+        public LawEnforcementDbContext(DbContextOptions<LawEnforcementDbContext> options) : base(options)
+        { }
+    }
+}
