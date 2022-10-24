117c380fa22546ff291abd24b692aa79b0e3ae5c
Piotr Bujak <piotr.bujak@motorolasolutions.com>
2022-10-24 14:23:31 +0200

    Add crud methods to crimeEventService

--- Rep_Crime.01_Crime.API/Program.cs
+++ Rep_Crime.01_Crime.API/Program.cs
@@ -1,7 +1,13 @@
+using Rep_Crime._01_Crime.API.Models;
+using Rep_Crime._01_Crime.API.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.Configure<CrimeEventsDatabaseSettings>(
+    builder.Configuration.GetSection("EventCrimeDatabaseSettings"));
 
+builder.Services.AddSingleton<CrimeEventService>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
--- Rep_Crime.01_Crime.API/Services/CrimeEventService.cs
+++ Rep_Crime.01_Crime.API/Services/CrimeEventService.cs
@@ -1,16 +1,44 @@
-﻿using Rep_Crime._01_Crime.API.Services.Interface;
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using Rep_Crime._01_Crime.API.Models;
+using Rep_Crime._01_Crime.API.Services.Interface;
 
 namespace Rep_Crime._01_Crime.API.Services
 {
     public class CrimeEventService : ICrimeEventService
     {
-        public CrimeEventService()
+        private readonly IMongoCollection<CrimeEvent> _crimeEventsCollection;
+        public CrimeEventService(IOptions<CrimeEventsDatabaseSettings> options)
         {
+            var mongoClient = new MongoClient(options.Value.ConnectionString);
+            var mongoDatabase = mongoClient.GetDatabase(options.Value.DatabaseName);
+            _crimeEventsCollection = mongoDatabase.GetCollection<CrimeEvent>(options.Value.CrimeCollectionName);
+        }
+
+        public async Task<List<CrimeEvent?>> GetAllEvents() =>
+       await _crimeEventsCollection.Find(_ => true).ToListAsync();
+
+
+        public async Task<CrimeEvent?> GetCrimeEventById(string id) =>
+        await _crimeEventsCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
+
+
+        public async Task<List<CrimeEvent?>> GetAllEventsByType(EventType eventType) =>
+        await _crimeEventsCollection.Find(x => x.EventType == eventType).ToListAsync();
+
+
+        public async Task CreateEventAsync(CrimeEvent newCrimeEvent) =>
+        await _crimeEventsCollection.InsertOneAsync(newCrimeEvent);
+
+
+        public async Task UpdateCrimeEventStatus(string id, CrimeEvent updatedCrimeEvent)
+
+        {
+            await _crimeEventsCollection.ReplaceOneAsync(x => x.Id == id, updatedCrimeEvent);
         }
 
 
-
-
-
+        public async Task RemoveAsync(string id) =>
+       await _crimeEventsCollection.DeleteOneAsync(x => x.Id == id);
     }
 }
